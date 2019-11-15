435051ea69ac3197ff66b16a4a162e877326996b
Konstantin Bondarev <Bondkr@mail.ru>
2019-11-15 23:12:30 +0300

    Builded the app
    Final final commit

---
+++ todo/build/precache-manifest.cdbe3f2177ee3c7660675934ee467994.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6188924b170b3bc09f55dfb0c57ac13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "daccb75539dfa9685dc6",
+    "url": "/static/css/main.31bf977f.chunk.css"
+  },
+  {
+    "revision": "1026e663629320b841f0",
+    "url": "/static/js/2.6c964e58.chunk.js"
+  },
+  {
+    "revision": "daccb75539dfa9685dc6",
+    "url": "/static/js/main.51b9c2d4.chunk.js"
+  },
+  {
+    "revision": "434872893da42ccf6f6f",
+    "url": "/static/js/runtime-main.9f9cf6c2.js"
+  }
+]);
