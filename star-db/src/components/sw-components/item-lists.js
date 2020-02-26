911418fb57d3f0efd96f3c3f5484b00d0dc77df6
Konstantin Bondarev <Bondkr@mail.ru>
2020-02-27 00:03:46 +0300

    More refactoring

--- star-db/src/components/sw-components/item-lists.js
+++ star-db/src/components/sw-components/item-lists.js
@@ -11,11 +11,22 @@ const {
     getAllPlanets,
 } = swapiService;
 
-const PersonList = withData(ItemList, getAllPeople);
+const withChildFunction = (Wrapped, fn) => {
+    return (props) => {
+        return(
+            <Wrapped {...props}>
+                {fn}
+            </Wrapped>
+        )
+    }
+};
 
-const PlanetList = withData(ItemList, getAllPlanets);
+const renderName = ({ name }) => <span>{name}</span>;
+const ListWithChildren = withChildFunction(ItemList, renderName);
 
-const StarshipList = withData(ItemList, getAllStarships);
+const PersonList = withData(ListWithChildren, getAllPeople);
+const PlanetList = withData(ListWithChildren, getAllPlanets);
+const StarshipList = withData(ListWithChildren, getAllStarships);
 
 export {
     PersonList,
