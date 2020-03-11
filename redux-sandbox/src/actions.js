7cd28c715ebd15eadae374b80cef6c6319b571b7
Konstantin Bondarev <Bondkr@mail.ru>
2020-03-11 20:02:42 +0300

    Redux sandbox finished

---
+++ redux-sandbox/src/actions.js
@@ -0,0 +1,11 @@
+export const inc = () => ({ type: 'INC' });
+
+export const dec = () => ({ type: 'DEC' });
+
+export const rnd = () => {
+
+    return {
+        type: 'RND',
+        payload: Math.ceil(Math.random()*10)
+    };
+};
