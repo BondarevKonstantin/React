1ca17c6eda36af72351de64e84341270c05940c0
Konstantin Bondarev <Bondkr@mail.ru>
2020-03-24 19:25:19 +0300

    Supporting components added

--- re-store/src/components/error-indicator/error-indicator.js
+++ re-store/src/components/error-indicator/error-indicator.js
@@ -2,12 +2,9 @@ import React from 'react';
 
 import './error-indicator.css';
 
-import icon from './';
-
 const ErrorIndicator = () => {
     return (
         <div className="error-indicator">
-            <img src={icon} alt='error icon' />
             <span className="error-header">Error</span>
             <span>Somethig's not right</span>
         </div>
